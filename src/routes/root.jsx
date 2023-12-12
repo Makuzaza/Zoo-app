db028167885482fc8f616eacf114593585565909
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-12-12 20:27:33 +0200

    new

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,9 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../components/header.jsx';
+import Footer from '../components/footer.jsx';
 import { Outlet } from 'react-router-dom';
 
 const Root = ({ cleanHandler }) => {
-  
+
     return (  
         <>  
     <Header/>

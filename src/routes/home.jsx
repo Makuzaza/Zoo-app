16f1e11b968380e41dfeca0d70b356402eb1dd33
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-12-13 10:10:22 +0200

    new

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../index.css'
 
 function Home () {
@@ -6,12 +6,12 @@ function Home () {
         <main >
         <div className="home">
             <div className='columns'>
-        <NavLink to='/animals'><div className="img_animals">Animals</div></NavLink>
-        <NavLink to='/fishes'><div className="img_fishes">Fishes</div></NavLink>
+        <Link to='/animals'><div className="img_animals">Animals</div></Link>
+        <Link to='/fishes'><div className="img_fishes">Fishes</div></Link>
             </div>
             <div className='columns'>
-        <NavLink to='/birds'><div className="img_birds">Birds</div></NavLink>
-        <NavLink to='/insects'><div className="img_insects">Insects</div></NavLink>
+        <Link to='/birds'><div className="img_birds">Birds</div></Link>
+        <Link to='/insects'><div className="img_insects">Insects</div></Link>
             </div>
         </div>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
